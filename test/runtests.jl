cc9ff52b09def299ce70a28f18269539fd870b99
luizpancini <luizpancini@usp.br>
2025-06-05 14:35:05 -0300

    debug CI with increased tolerance

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,14 +6,14 @@ SELFatol = 1e-4
 # Option to locally run tests failing in CI
 runCIfailingTests = false
 
-include("dynamicAeroelasticTests.jl")
+# include("dynamicAeroelasticTests.jl")
 include("eigenAeroelasticTests.jl")
-include("trimAeroelasticTests.jl")
-include("steadyAeroelasticTests.jl")
-include("aerodynamicTests.jl")
-include("dynamicStructuralTests.jl")
-include("trimStructuralTests.jl")
-include("modalTests.jl")
-include("staticStructuralTests.jl")
+# include("trimAeroelasticTests.jl")
+# include("steadyAeroelasticTests.jl")
+# include("aerodynamicTests.jl")
+# include("dynamicStructuralTests.jl")
+# include("trimStructuralTests.jl")
+# include("modalTests.jl")
+# include("staticStructuralTests.jl")
 
 println("Finished tests")
